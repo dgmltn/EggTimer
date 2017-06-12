e30ab8c3aa70fa5b38a42e5bf41c8c6551555a27
Doug Melton <doug@mobiata.com>
2017-06-12 13:56:42 -0400

    Made reviews use REST API instead of webhooks

--- eggtimer.js
+++ eggtimer.js
@@ -22,13 +22,14 @@ const GITHUB_AUTHENTICATION = { type: 'token', username: CONFIG.github_username,
 //         head_sha: 'abcd1234...',
 //         ref: 'my-pull-request',
 //         checks: {
-//             'context1': {
-//             },
-//             'context2': {
-//             },
-//         }
-//         review_state: 'pending|success|...',
-//         mergeable: true|false,
+//             'context1': true|false,
+//             'context2': true|false
+//         },
+//         reviews: {
+//             'id1': true|false,
+//             'id2': true|false
+//         },
+//         mergeable: true|false
 //     }
 // }
 var prs = {};
@@ -55,46 +56,50 @@ HANDLER.on('error', function (err) {
 });
 
 HANDLER.on('pull_request_review', function(event) {
-    var url = event.payload.pull_request.url;
-    var head_sha = event.payload.pull_request.head.sha;
-    var review_state = event.payload.review.state;
-    var ref = event.payload.pull_request.head.ref;
+    const url = event.payload.pull_request.url;
+    const head_sha = event.payload.pull_request.head.sha;
+    const id = event.payload.id;
+    const approved = event.payload.review.state.toLowerCase() == "approved";
+    const ref = event.payload.pull_request.head.ref;
     console.log(url + " -> pull_request_review");
     ensurePr(url, head_sha);
-    prs[url].review_state = review_state;
+    prs[url].reviews[id] = approved;
     prs[url].ref = ref;
     populateMergeable(url);
+    populateReviews(url);
     mergeIfReady(url);
 });
 
 HANDLER.on('pull_request', function(event) {
-    var url = event.payload.pull_request.url;
-    var head_sha = event.payload.pull_request.head.sha;
-    var ref = event.payload.pull_request.head.ref;
+    const url = event.payload.pull_request.url;
+    const head_sha = event.payload.pull_request.head.sha;
+    const ref = event.payload.pull_request.head.ref;
     console.log(url + " -> pull_request");
     ensurePr(url, head_sha);
     prs[url].ref = ref;
     populateMergeable(url);
+    populateReviews(url);
     mergeIfReady(url);
 });
 
 HANDLER.on('status', function(event) {
-    var sha = event.payload.sha;
-    var context = event.payload.context;
-    var state = event.payload.state;
+    const sha = event.payload.sha;
+    const context = event.payload.context;
+    const state = event.payload.state;
     if (sha in commits) {
-        var url = commits[sha];
+        const url = commits[sha];
         console.log(url + " -> status");
         ensurePr(url, sha);
         prs[url].checks[context] = state;
-        mergeIfReady(url);
     }
+    mergeIfReady(url);
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Private helpers
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Initialize an empty pr
 function ensurePr(url, head_sha) {
     if (!(url in prs)) {
         prs[url] = {};
@@ -102,66 +107,92 @@ function ensurePr(url, head_sha) {
     if (!('head_sha' in prs[url]) || prs[url].head_sha != head_sha) {
         prs[url].head_sha = head_sha;
         prs[url].checks = {};
+        prs[url].reviews = {};
+        prs[url].mergeable = false;
     }
     commits[head_sha] = url;
 }
 
+// GET pull requests and check their mergeable status
 function populateMergeable(url) {
-    var params = parsePullRequestUrl(url);
-    GITHUB.pullRequests.get(params,
+    setTimeout(function() {
+        const params = parsePullRequestUrl(url);
+        GITHUB.pullRequests.get(params,
+            function(err, pr) {
+                prs[url].mergeable = !!pr.data.mergeable;
+                mergeIfReady(url);
+            }
+        )
+    }, 10000);
+}
+
+// GET pr reviews and check their approved status
+function populateReviews(url) {
+    const params = parsePullRequestUrl(url);
+    GITHUB.pullRequests.getReviews(params,
         function(err, res) {
-            prs[url].mergeable = res.mergeable;
+            for (i in res.data) {
+                const review = res.data[i];
+                const id = review.id;
+                const approved = review.state.toLowerCase() == "approved";
+                prs[url].reviews[id] = approved;
+            }
+
             mergeIfReady(url);
         }
     );
 }
 
+// Perform a merge on this PR if:
+// 1. it's mergeable
+// 2. >1 reviews exist and all are approved
+// 3. >1 checks exist and all passed
 function mergeIfReady(url) {
     console.log(JSON.stringify(prs, null, 4));
     if (url in prs 
         && !prs[url].done
-        && isApproved(prs[url])
         && isMergeable(prs[url])
+        && isApproved(prs[url])
         && checksPassed(prs[url])) {
 
         // APPROVED!
         prs[url].done = true;
         console.log("APPROVED!");
 
-        mergePullRequest(url, 
-            function(err, res) {
-                if (err) {
-                    console.log("Error: could not merge: " + err);
-                    delete prs[url].done;
-                    return;
-                }
-                console.log("MERGED!");
-
-                if (CONFIG.delete_after_merge) {
-                    deleteReference(url,
-                        function(err, res) {
-                            if (err) {
-                                console.log("Error: could not delete ref: " + err);
-                                return;
-                            }
-                            console.log("DELETED!");
-                        }
-                    );
-                }
+        const deleteCallback = function(err, res) {
+            if (err) {
+                console.log("Error: could not delete ref: " + err);
+                return;
             }
-        );
+            console.log("DELETED!");
+        };
+
+        const mergeCallback = function(err, res) {
+            if (err) {
+                console.log("Error: could not merge: " + err);
+                delete prs[url].done;
+                return;
+            }
+            console.log("MERGED!");
+
+            if (CONFIG.delete_after_merge) {
+                deleteReference(url, deleteCallback);
+            }
+        };
+
+        mergePullRequest(url, mergeCallback);
     }
 }
 
 function mergePullRequest(url, callback) {
-    var params = parsePullRequestUrl(url);
+    const params = parsePullRequestUrl(url);
     params.sha = prs[url].head_sha;
     GITHUB.authenticate(GITHUB_AUTHENTICATION);
     GITHUB.pullRequests.merge(params, callback);
 }
 
 function deleteReference(url, callback) {
-    var params = parsePullRequestUrl(url);
+    const params = parsePullRequestUrl(url);
     params.ref = 'heads/' + prs[url].ref;
     GITHUB.authenticate(GITHUB_AUTHENTICATION);
     GITHUB.gitdata.deleteReference(params, callback);
@@ -169,7 +200,7 @@ function deleteReference(url, callback) {
 
 function parsePullRequestUrl(url) {
     const re = /^https?:\/\/([^\/]+)\/repos\/([^\/]+)\/([^\/]+)\/pulls\/(\d+)$/;
-    var match = re.exec(url);
+    const match = re.exec(url);
     return {
         owner: match[2],
         repo: match[3],
@@ -177,12 +208,23 @@ function parsePullRequestUrl(url) {
     };
 }
 
-function isApproved(obj) {
-    return 'review_state' in obj && obj.review_state == 'approved';
+function isMergeable(obj) {
+    return 'mergeable' in obj && !!obj.mergeable;
 }
 
-function isMergeable(obj) {
-    return 'mergeable' in obj && obj.mergeable == true
+function isApproved(obj) {
+    if (!('reviews' in obj)) {
+        return false;
+    }
+    else if (Object.keys(obj.reviews).length <= 0) {
+        return false;
+    }
+    for (var id in obj.reviews) {
+        if (!obj.reviews[id]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function checksPassed(obj) {
@@ -193,7 +235,7 @@ function checksPassed(obj) {
         return false;
     }
     for (var context in obj.checks) {
-        if (obj.checks[context] != 'success') {
+        if (!obj.checks[context]) {
             return false;
         }
     }
